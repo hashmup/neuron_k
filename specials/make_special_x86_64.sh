9f0183e4b32c41a1da3a5dbaf99272fa918126d2
hashmup <hashmup@gmail.com>
2018-01-01 15:50:23 +0900

    Change the meaning of the flag

--- specials/make_special_x86_64.sh
+++ specials/make_special_x86_64.sh
@@ -6,15 +6,17 @@ rm -r ${ARCH}
 ../exec/${ARCH}/bin/nrnivmodl ../mod
 if [ $# -eq 0 ]
 then
-    echo "bench"
+    echo "optimized"
+    rm ./${ARCH}/hh_k.c
+    cp ~/genie/genie/transpiler/tmp/hh_k.c ${ARCH}/hh_k.c
 else
     if [ $1 == 'True' ]
     then
-        echo "genie"
+        echo "bench"
+    else
+        echo "optimized"
         rm ./${ARCH}/hh_k.c
         cp ~/genie/genie/transpiler/tmp/hh_k.c ${ARCH}/hh_k.c
-    else
-        echo "bench"
         #rm ./${ARCH}/hh_k.c
         #cp ./hh_k7_3.c ${ARCH}/hh_k.c
     fi
