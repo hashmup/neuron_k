e8700bfbc8ec5653b49600ada54a3dbe2abc9b19
hashmup <hashmup@gmail.com>
2018-01-11 11:35:48 +0900

    make it work for both k and cluster

---
+++ specials/make_special.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+ARCH=$1
+
+rm -r ${ARCH}
+../exec/${ARCH}/bin/nrnivmodl ../mod
+if [ $# -eq 1 ]
+then
+    echo "optimized"
+    rm ./${ARCH}/hh_k.c
+    cp ~/genie/genie/transpiler/tmp/hh_k.c ${ARCH}/hh_k.c
+else
+    if [ $2 == 'True' ]
+    then
+        echo "bench"
+    else
+        echo "optimized"
+        rm ./${ARCH}/hh_k.c
+        cp ~/genie/genie/transpiler/tmp/hh_k.c ${ARCH}/hh_k.c
+        #rm ./${ARCH}/hh_k.c
+        #cp ./hh_k7_3.c ${ARCH}/hh_k.c
+    fi
+fi
+../exec/${ARCH}/bin/nrnivmodl ../mod
